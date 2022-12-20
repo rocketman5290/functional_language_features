8a4cf34734d7b6bcb6e95dfe263c50d0d13e3dd6
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-20 15:23:46 -0500

    Add: different ways to acheive the same thing with functions and closures

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::Duration;
 
+//replaced by closure aka anonymous function.
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("Calculating slowly...");
     thread::sleep(Duration::from_secs(2));
@@ -13,8 +14,9 @@ fn generate_workout(intensity: u32, random_number: u32) {
         thread::sleep(Duration::from_secs(2));
         num
     };
+
     // replace with closure as the value of expensive_closure variable 
-    //    which stores the annoynemous function until needed and then is called,
+    //    which stores the anonymous function until needed and then is called,
     //    returning the side effects defined and the return value num at the end.
     
     // let expensive_result = simulated_expensive_calculation(intensity);
@@ -43,7 +45,18 @@ fn generate_workout(intensity: u32, random_number: u32) {
     }
 }
 fn main() {
-    println!("Hello, world!");
+    println!("examples of all the same work in function and then closure form");
+    fn add_one_v1(x:u32) -> u32 {x + 1}
+    let add_one_v2 = |x: u32| -> u32 {x + 1};
+    let add_one_v3 = |x| { x + 1 };
+    let add_one_v4 = |x| x + 1;
+    //print above versions of same logic.
+    println!("{}", add_one_v1(1));
+    println!("{}", add_one_v2(1));
+    println!("{}", add_one_v3(1));
+    println!("{}", add_one_v4(1));
+
+
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
