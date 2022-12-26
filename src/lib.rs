063940ddb337ed5d06ea6d1518ea8298cd429801
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-25 20:41:42 -0500

    add: struct Counter that implements Iterator trait and then add test to make sure the use of next on a new Counter instance works as expected

--- src/lib.rs
+++ src/lib.rs
@@ -38,11 +38,49 @@ fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0}
+    }
+}
+
+//Implementing Iterator trait for counter which allows us to use 
+//associated iterator functions.
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use std::vec;
 
     use super::*;
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
 
     #[test]
     fn filter_by_size() {
